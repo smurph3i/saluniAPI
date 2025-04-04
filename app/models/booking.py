6ea8098a002d55204cfd3d99d901ac455490c3f3
Murphy <smurph3iii@gmail.com>
2025-04-04 18:32:37 +1100

    Created the booking model

--- app/models/booking.py
+++ app/models/booking.py
@@ -1,1 +1,12 @@
 from sqlalchemy import Column, Integer, DateTime
+from database import Base
+
+
+class BookingDB(Base):
+    __tablename__ = "booking"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    booking_type = Column(Integer)
