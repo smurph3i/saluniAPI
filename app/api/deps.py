70163bffd4d4d88e53ab18b0d4916f98ddd5f5ea
Murphy <smurph3iii@gmail.com>
2025-05-03 19:01:09 +1000

    created a deps file and established a get_db function

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,9 +1,9 @@
 from typing import Generator
+from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 
 
-def get_db() -> Generator:
-    """ Access our database"""
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
--- app/db/session.py
+++ app/db/session.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# echo=True for SQL logging
+engine = create_engine(settings.database_url, echo=True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
